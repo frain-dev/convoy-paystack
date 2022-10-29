e37f8c6e99034f7113c54e32d392ff97df3a0795
Subomi Oluwalana <subomioluwalana71@gmail.com>
2022-10-28 19:41:15 -0700

    feat: test convoy-go for advanced verification

--- serverless-function/functions.go
+++ serverless-function/functions.go
@@ -5,7 +5,9 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 
+	client "github.com/frain-dev/convoy-go"
 	"github.com/frain-dev/convoy/pkg/verifier"
 	"github.com/go-chi/chi/v5"
 	log "github.com/sirupsen/logrus"
@@ -21,7 +23,11 @@ var (
 	// Retrieve Header
 	header = os.Getenv("CONVOY_HEADER")
 
-	pv = getConvoyVerifier()
+	// Configure Paystack Verifier
+	pv = getPaystackVerifier()
+
+	// Configure Convoy Verifier
+	cv = getConvoyVerifier()
 )
 
 // WebhookEndpoint is a HTTP Function to receive events from the world.
@@ -31,20 +37,23 @@ func WebhookEndpoint(w http.ResponseWriter, r *http.Request) {
 
 	router.Route("/v1", func(v1Router chi.Router) {
 
-		v1Router.Post("/webhooks/{provider}", WebhooksHandler)
+		v1Router.Post("/webhooks/simple", SimpleWebhooksHandler)
+		v1Router.Post("/webhooks/advanced", AdvancedWebhooksHandler)
 	})
 
 	// Serve Request.
 	router.ServeHTTP(w, r)
 }
 
-func WebhooksHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Header)
+func SimpleWebhooksHandler(w http.ResponseWriter, r *http.Request) {
+	// Log Request.
+	log.Info(r.Header)
+
 	// Read Request.
 	payload, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		log.WithError(err).Error("Bad Request: Could not read payload")
+		fmt.Fprintln(w, err)
 		return
 	}
 
@@ -52,7 +61,7 @@ func WebhooksHandler(w http.ResponseWriter, r *http.Request) {
 	err = pv.VerifyRequest(r, payload)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		log.WithError(err).Error("Bad Request: Could not verify request")
+		fmt.Fprintln(w, err)
 		return
 	}
 
@@ -62,11 +71,50 @@ func WebhooksHandler(w http.ResponseWriter, r *http.Request) {
 	// Perform business logic.
 }
 
-func getConvoyVerifier() verifier.Verifier {
+func AdvancedWebhooksHandler(w http.ResponseWriter, r *http.Request) {
+	// Log Request.
+	log.Info(r.Header)
+
+	// Read Request.
+	payload, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	// Verify Request
+	cv.Payload = payload
+	err = cv.Verify()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	// Respond.
+	w.Write([]byte("Event received."))
+
+	// Perform business logic.
+}
+
+func getPaystackVerifier() verifier.Verifier {
 	return verifier.NewHmacVerifier(&verifier.HmacOptions{
 		Header:   header,
 		Hash:     hash,
 		Secret:   convoySecret,
 		Encoding: "hex",
 	})
+}
+
+func getConvoyVerifier() *client.Webhook {
+	opts := client.ConfigOpts{
+		SigHeader: header,
+		Hash:      hash,
+		Secret, convoySecret,
+		IsAdvanced: true,
+		Encoding:   client.HexEncoding,
+		Tolerance:  5 * time.Duration,
+	}
+	return &client.NewWebhook(&opts)
 }
--- serverless-function/go.mod
+++ serverless-function/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/GoogleCloudPlatform/functions-framework-go v1.6.1
 	github.com/frain-dev/convoy v0.7.0
+	github.com/frain-dev/convoy-go v0.3.2
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/sirupsen/logrus v1.9.0
 )
